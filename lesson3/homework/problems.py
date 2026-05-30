a371a65cb9115d1eebf31fcd9cc0d95a1db109e7
kyleepinto-dot <kylee.pinto@gmail.com>
2026-05-30 03:15:05 +0000

    Homework for lessons 3 and 4 done

--- lesson3/homework/problems.py
+++ lesson3/homework/problems.py
@@ -1,27 +1,59 @@
 # Problem 1
 # Ask the user to enter a number.
 # Print "Even" if the number is divisible by 2, otherwise print "Odd".
-
-
-
+number = int(input("Enter a number: "))
+if number % 2 == 0:
+  print("Even")
+else:
+  print("Odd")
 # Problem 2
 # Ask the user for the day of the week (all lowercase).
 # Print "Weekend" if the day is "saturday" or "sunday",
 # else print "Weekday".
-
-
-
+day_of_week = input("Enter the date:").lower()
+if day_of_week == "sunday" or "saterday":
+    print("Weekend")
+else:
+   print("Weekday")
 # Problem 3
 # Generate a random number between 1 and 10 (inclusive).
 # Ask the user to guess the number.
 # Print "Correct!" if the guess matches the random number, else print "Try again!".
+import random
+
+def guess_number():
+    secret_number = random.randint(1, 10)
+
+    try:
+        guess = int(input("Guess a number between 1 and 10: "))
+
+        if guess < 1 or guess > 10:
+            print("Please enter a number between 1 and 10.")
+            return
+
+        if guess == secret_number:
+            print("Correct!")
+        else:
+            print(f"Try again! The number was {secret_number}.")
+    except ValueError:
+        print("Invalid input. Please enter an integer.")
+
+if __name__ == "__main__":
+    guess_number()
 
 
 
 # Problem 4
 # Ask the user for a positive integer.
 # If the number is divisible by 2 and greater than 10, print "Big even number".
-# Otherwise print "Number does not meet criteria".
+# Otherwise print "Number does not meet criteria."
+nums = int(input("Enter a positive number:"))
+if nums % 2 == 0 and nums > 10:
+    print("Big even number.")
+else:
+    print("Number does not meet criteria.")
+
+
 
 
 
@@ -29,3 +61,11 @@
 # Ask the user for two numbers.
 # Print which number is larger.
 # If the numbers are equal, print "Numbers are equal".
+num_1 = int(input("Enter first number:"))
+num_2 = int(input("Enter second number:"))
+if num_1 > num_2:
+    print("First number is bigger.")
+elif num_2 > num_1:
+    print("Second number is bigger.")
+else:
+    print("Numbers are equal.")
--- lesson4/homework/problems.py
+++ lesson4/homework/problems.py
@@ -2,6 +2,12 @@
 # Ask user for two test scores.
 # If BOTH scores are at least 50, print "You passed both!"
 # Otherwise, print "You failed at least one."
+test_1 = int(input("Enter 1st test score:"))
+test_2 = int(input("Enter 2nd test score:"))
+if test_1 >= 50 and test_2 >= 50:
+    print("You passed both!")
+else:
+    print("You failed at least one.")
 
 
 
@@ -10,6 +16,16 @@
 # If they brought lunch OR water, print "You're somewhat ready."
 # If they brought both, print "You're fully ready!"
 # If they brought neither, print "You're not ready."
+lunch = input("Did you bring lunch? (yes/no): ").strip().lower()
+water = input("Did you bring water? (yes/no): ").strip().lower()
+
+if lunch == "yes" and water == "yes":
+    print("You're fully ready!")
+elif lunch == "yes" or water == "yes":
+    print("You're somewhat ready.")
+else:
+    print("You're not ready.")
+
 
 
 
@@ -17,6 +33,11 @@
 # Ask user to enter a number.
 # If the number is NOT between 1 and 10 (inclusive), print "Out of range."
 # Otherwise, print "In range."
+nums = int(input("Enter a number:"))
+if nums in range(1,11,1):
+    print("In range.")
+else:
+    print("Out of range.")
 
 
 
@@ -26,10 +47,31 @@
 # If the guess is right AND the number is even, print "Even match!"
 # Else if guess is right OR number is 5, print "Nice try!"
 # Otherwise, print "Nope."
+import random
 
+secret_number = random.randint(1, 10)
+
+guess = int(input("Guess a number between 1 and 10: "))
+
+if guess == secret_number and secret_number % 2 == 0:
+    print("Even match!")
+elif guess == secret_number or secret_number == 5:
+    print("Nice try!")
+else:
+    print("Nope.")
 
 
 # Problem 5
 # Ask the user for two numbers.
 # If one is divisible by 5 AND the other is NOT divisible by 2, print "Interesting pair!"
 # Otherwise, print "Plain pair."
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+cond1 = (num1 % 5 == 0 and num2 % 2 != 0)
+cond2 = (num2 % 5 == 0 and num1 % 2 != 0)
+
+if cond1 or cond2:
+    print("Interesting pair!")
+else:
+    print("Plain pair.")
