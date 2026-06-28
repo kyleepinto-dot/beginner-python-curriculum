5fb69efe669e65570fd23064b6e2dcc1f4d68a1c
kyleepinto-dot <kylee.pinto@gmail.com>
2026-06-28 01:35:38 +0000

    practice and homework for lesson 7

--- lesson7/classwork/practice.py
+++ lesson7/classwork/practice.py
@@ -2,11 +2,24 @@
 # Find and print the total sum of all the numbers in the list.
 numbers = [4, 11, 22, -6, 3]
 
+total = 0
+for num in numbers:
+    total += num
+
+print(total)
+
 
 
 # Problem 2
 # Find and print the biggest number in the list.
 numbers = [-9, 17, 5, -3, 0]
+
+max_num = float('-inf')
+for num in numbers:
+    if num > max_num:
+        max_num = num
+
+print(max_num)
 
 
 
@@ -14,14 +27,37 @@ numbers = [-9, 17, 5, -3, 0]
 # Find and print the sum of only the negative numbers in the list (negative means less than 0).
 numbers = [2, -1, 8, 10, -7, 6]
 
+total = 0
+for num in numbers:
+    if num < 0:
+        total += num
+
+print(total)
+
 
 
 # Problem 4
 # Find and print the sum of only the even numbers in the list. 
 numbers = [8, 3, 15, 22, 11, 6]
 
+total = 0
+for num in numbers:
+    if num % 2 == 0:
+        total += num
+
+print(total)
+
+
+
 
 
 # Problem 5
 # Find and print the biggest number that is negative in the list.
 numbers = [-1, -30, -5, 7, 12, -2]
+
+max_neg = float('-inf')
+for num in numbers:
+    if num < 0 and num > max_neg:
+        max_neg = num
+
+print(max_neg)
--- lesson7/homework/problems.py
+++ lesson7/homework/problems.py
@@ -2,26 +2,56 @@
 # Find and print the sum of all the numbers greater than 25 in the list.
 numbers = [10, 32, 27, 8, 50]
 
+total = 0
+for num in numbers:
+    if num > 25:
+        total += num
 
+print(total)
 
 # Problem 2
 # Find and print the sum of all the numbers less than -10 in the list.
 numbers = [-5, -20, -11, 0, 4, -15]
 
+total = 0
+for num in numbers:
+    if num < -10:
+        total += num
+
+print(total)
 
 
 # Problem 3
 # Find and print the biggest number less than 100 in the list.
 numbers = [104, 99, 86, 120, 101]
 
+max_num = float('-inf')
+for num in numbers:
+    if num < 100 and num > max_num:
+        max_num = num
+
+print(max_num)
 
 
 # Problem 4
 # Find and print the biggest number in the list.
 numbers = [12, 7, 33, 5]
 
+max_num = float('-inf')
+for num in numbers:
+    if num > max_num:
+        max_num = num
+
+print(max_num)
+
 
 
 # Problem 5
 # Find and print the total sum of all the numbers in the list.
 numbers = [1, 3, 5, 7, 9]
+
+total = 0
+for num in numbers:
+    total += num
+
+print(total)
